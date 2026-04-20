bab6c0128285c05338c874dadb6451ad72c0fc67
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-04-20 09:32:55 +0200

    implementering af GET

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,31 @@
 import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
 
-const starterProducts = [
-  {
-    id: "starter-1",
-    title: "Starter Product",
-    price: 0,
-    image: "",
-  },
-];
+// env-værdier i variabler
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const APIKEY = import.meta.env.VITE_SUPABASE_APIKEY;
 
 export default function HomePage() {
-  // TODO (Trin 1): Gem env-værdier i variabler, fx:
-  const URL = import.meta.env.VITE_SUPABASE_URL;
-  const APIKEY = import.meta.env.VITE_SUPABASE_APIKEY;
-  // TODO (Trin 2): Implementer GET i HomePage med useEffect/useState og fetch.
-  const products = starterProducts;
+  // state til products
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await fetch(URL, {
+        headers: {
+          apikey: APIKEY,
+          "content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setProducts(data);
+    }
+    loadProducts();
+  }, []);
 
   return (
     <main className="app">
       <h1 className="page-title">All Products</h1>
-      <p className="status-msg">
-        TODO: Implement GET products with fetch and replace starter data.
-      </p>
       <section className="product-list">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
