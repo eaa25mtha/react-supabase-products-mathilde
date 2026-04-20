1137a063e66df6f9036272dabc2d4653cc412d9e
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-04-20 09:51:00 +0200

    opdateret .env med de rigtige værdier fra egen database

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,12 +1,23 @@
 import { useNavigate } from "react-router";
 import ProductForm from "../components/ProductForm";
 
+// env-værdier i variabler
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const APIKEY = import.meta.env.VITE_SUPABASE_APIKEY;
+
 export default function CreatePage() {
   const navigate = useNavigate();
 
   async function handleSubmit(productData) {
-    console.log("CreatePage productData:", productData);
-    // TODO (Trin 3): Implementer POST med fetch, URL/APIKEY og body fra productData.
+    //implementering af POST med fetch og data fra productData
+    await fetch(URL, {
+      method: "POST",
+      headers: {
+        apikey: APIKEY,
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
     navigate("/");
   }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,6 +17,7 @@ export default function HomePage() {
           "content-Type": "application/json",
         },
       });
+      //konvertering af data til json
       const data = await response.json();
       setProducts(data);
     }
